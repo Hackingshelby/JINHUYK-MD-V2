2b659be95d3d5e464ab80cb75a2d8188d252d90f
Hackingshelby <hackingthomas00@gmail.com>
2025-01-11 15:46:02 +0100

    Update config.js

--- config.js
+++ config.js
@@ -28,17 +28,17 @@ module.exports = {
 
 SESSION_ID: process.env.SESSION_ID || "YOUR JINHUYK SESSION PLACE IT HERE",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", //TRUE OR FALSE
-OWNER_NUMBER: process.env.OWNER_NUMBER || "242067274660",
-MODE: process.env.MODE || "public",  // PRIVATE OR PUBLIC
+OWNER_NUMBER: process.env.OWNER_NUMBER || "243904468847",
+MODE: process.env.MODE || "private",  // PRIVATE OR PUBLIC
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.postimg.cc/cLL0MWyT/JINHUYK-MD-V2.jpg", // DO NOT CHANGE
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/552Y0Yq/Manul-Ofc-X.jpg", // DO NOT CHANGE
 ALIVE_MSG: process.env.ALIVE_MSG || "𝙹𝙸𝙽𝙷𝚄𝚈𝙺 ᴀɪ ɪs ᴀʟɪᴠᴇ. ᴋᴇᴇᴘ sᴜᴘᴘᴏʀᴛɪɴɢ *𝚂𝙰𝚂𝙰𝙺𝙸-𝙲𝙾𝙼𝙿𝙰𝙶𝙽𝙸𝙴🍂*\n\n> ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ 𝙺𝙰𝙽𝙶 𝙹𝙸𝙽𝙷𝚄𝚈𝙺",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
 PREFIX: process.env.PREFIX || ".", // .,!/?
-FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
+FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
 AUTO_REACT: process.env.AUTO_REACT || "false",
 HEART_REACT: process.env.HEART_REACT || "false",
 OWNER_REACT: process.env.OWNER_REACT || "false",
